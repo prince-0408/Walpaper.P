4aa2aff3f6e14490a147844fd4b2f55ee67692af
Prince-0408 <princekuyadav5646@gmail.com>
2023-07-07 00:16:57 +0530

    Initial Commit

---
+++ Walpaper.P/Views/WalpaperView.swift
@@ -0,0 +1,20 @@
+//
+//  WalpaperView.swift
+//  Walpaper.P
+//
+//  Created by Prince Yadav on 07/07/23.
+//
+
+import SwiftUI
+
+struct WalpaperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WalpaperView_Previews: PreviewProvider {
+    static var previews: some View {
+        WalpaperView()
+    }
+}
---
+++ Walpaper.P/Walpaper_PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Walpaper_PApp.swift
+//  Walpaper.P
+//
+//  Created by Prince Yadav on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Walpaper_PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
